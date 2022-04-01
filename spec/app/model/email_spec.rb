60d9645c2bb589b0948fcb250975febd9a828993
yamashush <38120991+yamashush@users.noreply.github.com>
2022-04-01 11:44:29 +0900

    :tada: テストが完全に動いた

--- spec/app/model/email_spec.rb
+++ spec/app/model/email_spec.rb
@@ -5,29 +5,25 @@ require_relative '../../../app/model/email'
 
 describe Email do
   context "正常系" do
-    let(:row_params) { "hoge" }
-    # let(:row_params) do
-    #   <<~EMAIL_BODY
-    #   To:【U9999999999】株式会社smarthr hoge piyo 様<BR>
-    #   <BR>
-    #   平素は格別なご高配を賜り、厚く御礼申し上げます。株式会社SmartHRです。<BR>
-    #   以下のとおり入館申請を行いました。<BR>
-    #   I have registered your admission application as below.<BR>
-    #   =========================================================<BR>
-    #   <BR>
-    #   ■　ご来訪日時/Date<BR>
-    #   2022/03/31(Thu) 18:00<BR>
-    #   <BR>
-    #   ■　ビル/Building<BR>
-    #   住友不動産六本木グランドタワー/SUMITOMO FUDOSAN ROPPONGI GRAND TOWER<BR>
-    #   <BR>
-    #   ■　バーコード/Barcode<BR>
-    #   入館ID/Guest ID:12345678901<BR>
-    #   EMAIL_BODY
-    # end
+    email_html_body = <<~EMAIL_BODY
+      To:【U9999999999】株式会社smarthr hoge piyo 様<BR>
+      <BR>
+      平素は格別なご高配を賜り、厚く御礼申し上げます。株式会社SmartHRです。<BR>
+      以下のとおり入館申請を行いました。<BR>
+      I have registered your admission application as below.<BR>
+      =========================================================<BR>
+      <BR>
+      ■　ご来訪日時/Date<BR>
+      2022/03/31(Thu) 18:00<BR>
+      <BR>
+      ■　ビル/Building<BR>
+      住友不動産六本木グランドタワー/SUMITOMO FUDOSAN ROPPONGI GRAND TOWER<BR>
+      <BR>
+      ■　バーコード/Barcode<BR>
+      入館ID/Guest ID:12345678901<BR>
+    EMAIL_BODY
 
-    # email = described_class.new(row_params)
-    email = Email.new("boge")
+    email = described_class.new(email_html_body)
 
     it "slack_id" do
       expect(email.slack_id).to eq nil
