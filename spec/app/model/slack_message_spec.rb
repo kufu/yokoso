c2ad14710f03841c696d75422f4c07d791d64bbc
yamashush <38120991+yamashush@users.noreply.github.com>
2022-12-31 21:46:06 +0900

    :white_check_mark: Add test

--- spec/app/model/slack_message_spec.rb
+++ spec/app/model/slack_message_spec.rb
@@ -1,6 +1,7 @@
 # frozen_string_literal: true
 
 require "spec_helper"
+require_relative "../../../app/model/email"
 require_relative "../../../app/model/slack_dialog_submission"
 require_relative "../../../app/model/slack_message"
 
@@ -55,6 +56,47 @@ describe SlackMessage do
           { short: true, title: "訪問日時", value: "2023/01/01 08:00" }
         ]
         expect(instance.send(:received_message_attachment_fields)).to eq expected
+      end
+    end
+  end
+  describe "#notification_message_attachment_fields" do
+    context "ok" do
+      it do
+        email_fixture = <<~EMAIL_BODY
+          To:【U9999999999】株式会社smarthr hoge piyo 様<BR>
+          <BR>
+          平素は格別なご高配を賜り、厚く御礼申し上げます。株式会社SmartHRです。<BR>
+          以下のとおり入館申請を行いました。<BR>
+          I have registered your admission application as below.<BR>
+          =========================================================<BR>
+          <BR>
+          ■　ご来訪日時/Date<BR>
+          2022/03/31(Thu) 18:00<BR>
+          <BR>
+          ■　ビル/Building<BR>
+          住友不動産六本木グランドタワー/SUMITOMO FUDOSAN ROPPONGI GRAND TOWER<BR>
+          <BR>
+          ■　バーコード/Barcode<BR>
+          入館ID/Guest ID:12345678901<BR>
+          <BR>
+          　　<img src='12345678901.BMP' width='200'><BR>
+          <BR>
+        EMAIL_BODY
+        email = Email.new(email_fixture)
+        instance = described_class.new(email: email)
+
+        expected = [
+          { title: "来訪者名",
+            value: " 様",
+            short: true },
+          { title: "訪問日時",
+            value: "2022/03/31(Thu) 18:00",
+            short: true },
+          { title: "入館ID",
+            value: "12345678901",
+            short: true }
+        ]
+        expect(instance.send(:notification_message_attachment_fields)).to eq expected
       end
     end
   end
