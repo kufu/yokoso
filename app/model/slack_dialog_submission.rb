6d23b6d307bf1c6fd9129f7133d5cde8456d38f1
yamashush <38120991+yamashush@users.noreply.github.com>
2022-12-31 16:31:50 +0900

    :seedling: Add several method

--- app/model/slack_dialog_submission.rb
+++ app/model/slack_dialog_submission.rb
@@ -10,4 +10,14 @@ class SlackDialogSubmittion
   def slack_user_id
     @post_body.dig(:user, :id)
   end
+
+  # @return [String]
+  def slack_channel_id
+    @post_body.dig(:channel, :id)
+  end
+
+  # @return [String]
+  def recept_date
+    @post_body.dig(:submission, :date)
+  end
 end
