a5492a3ee3f4c88ef3cf6cb658e9c125ae165578
yamashush <38120991+yamashush@users.noreply.github.com>
2022-12-31 19:30:17 +0900

    :bulb: Add method yard comment

--- app/model/slack_message.rb
+++ app/model/slack_message.rb
@@ -2,12 +2,20 @@
 
 # @see 
 class SlackMessage
+  # @param fields [Array]  attachment_field
+  # @param color  [String] hex color code (eg. #439FE0)
+  # @return [Hash]
   # @see https://api.slack.com/reference/messaging/attachments
   def attachments(fields:, color: "good")
     { color: color,
       fields: fields }
   end
 
+  # @param title [Array]
+  # @param value [String]
+  # @param short [Boolean]
+  # @return [Hash]
+  # @see https://api.slack.com/reference/messaging/attachments
   def attachment_field(title:, value:, short: true)
     { title: title,
       value: value,
