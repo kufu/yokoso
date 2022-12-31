a6c492156e74d551c2060959692d6ad51cf60fa4
yamashush <38120991+yamashush@users.noreply.github.com>
2022-12-31 16:18:07 +0900

    :seedling: Add slack dialog submission class

---
+++ app/model/slack_dialog_submission.rb
@@ -0,0 +1,6 @@
+# frozen_string_literal: true
+
+# @see https://api.slack.com/dialogs
+class SlackDialogSubmittion
+
+end
---
+++ spec/app/model/slack_dialog_submission_spec.rb
@@ -0,0 +1,8 @@
+# frozen_string_literal: true
+
+require "spec_helper"
+require_relative "../../../app/model/slack_dialog_submission"
+
+describe SlackDialogSubmittion do
+
+end
