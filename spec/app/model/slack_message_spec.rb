856482928659694504e92b0c59e82497400ce247
yamashush <38120991+yamashush@users.noreply.github.com>
2022-12-31 19:15:29 +0900

    :white_check_mark: Add test

--- spec/app/model/slack_message_spec.rb
+++ spec/app/model/slack_message_spec.rb
@@ -3,4 +3,16 @@
 require "spec_helper"
 require_relative "../../../app/model/slack_message"
 describe SlackMessage do
+  let(:instance) { described_class.new }
+
+  describe "#attachment_field" do
+    context "ok" do
+      it do
+        expected = { title: "title",
+                     value: "タイトル",
+                     short: true }
+        expect(instance.send(:attachment_field)).to eq expected
+      end
+    end
+  end
 end
