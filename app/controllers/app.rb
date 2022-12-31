e2951030465a47bbacde341e56effc1335849b4b
yamashush <38120991+yamashush@users.noreply.github.com>
2022-12-31 12:10:14 +0900

    :zap: Autoloading sinatra for development

--- app/controllers/app.rb
+++ app/controllers/app.rb
@@ -1,12 +1,17 @@
 # frozen_string_literal: true
 
 require "sinatra/base"
+require "sinatra/reloader"
 require_relative "./slack_dialog"
 require_relative "./slack_interactive_message"
 require_relative "./slack_notification"
 
 class Yokoso < Sinatra::Base
   helpers SlackDialog, SlackInteractiveMessage, SlackNotification
+
+  configure :development do
+    register Sinatra::Reloader
+  end
 
   post("/dialog") do
     SlackDialog.run(request)
