1300a96455a85aa54e6843e3737dc43d2aa55983
yamashush <38120991+yamashush@users.noreply.github.com>
2022-12-31 16:22:48 +0900

    :white_check_mark: Add test

--- spec/app/model/slack_dialog_submission_spec.rb
+++ spec/app/model/slack_dialog_submission_spec.rb
@@ -4,5 +4,12 @@ require "spec_helper"
 require_relative "../../../app/model/slack_dialog_submission"
 
 describe SlackDialogSubmittion do
+  let(:instance) { described_class.new(post_body) }
 
+  describe "#recept_date" do
+    context "ok" do
+      let(:post_body) { { submission: { date: "2023/01/01" } } }
+      it { expect(instance.recept_date).to eq "2023/01/01" }
+    end
+  end
 end
