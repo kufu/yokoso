04b8d3d348ee59107a6a2fe2339fadb918d0f47f
yamashush <38120991+yamashush@users.noreply.github.com>
2022-12-31 16:59:30 +0900

    :necktie: Use DialogSubmission class

--- app/workers/reception_worker.rb
+++ app/workers/reception_worker.rb
@@ -3,12 +3,14 @@
 require "mechanize"
 require "sidekiq"
 require "slack-ruby-client"
+require_relative "../model/slack_dialog_submission"
 
 class ReceptionWorker
   include Sidekiq::Worker
   sidekiq_options queue: :default, retry: false
 
   def perform(slack_dialog)
+    dialog_result = SlackDialogSubmission.new(slack_dialog)
     # slack dialog input
     recept_date = slack_dialog["submission"]["date"]
     recept_time = slack_dialog["submission"]["time"]
@@ -17,32 +19,32 @@ class ReceptionWorker
     slack_id = slack_dialog["user"]["id"]
     slack_channel = slack_dialog["channel"]["id"]
 
-    # srd-gate login
-    agent = Mechanize.new
-    agent.get("https://srd-gate.com/03/login.cgi")
-    agent.page.form.field_with(name: "userid").value = ENV.fetch("SRD_GATE_USERNAME")
-    agent.page.form.field_with(name: "passwd").value = ENV.fetch("SRD_GATE_PASSWORD")
-    agent.page.form.submit
+    # # srd-gate login
+    # agent = Mechanize.new
+    # agent.get("https://srd-gate.com/03/ login.cgi")
+    # agent.page.form.field_with(name: "userid").value = ENV.fetch("SRD_GATE_USERNAME")
+    # agent.page.form.field_with(name: "passwd").value = ENV.fetch("SRD_GATE_PASSWORD")
+    # agent.page.form.submit
 
-    # srd-gate regist page
-    agent.get("https://srd-gate.com/03/sinsei.cgi")
+    # # srd-gate regist page
+    # agent.get("https://srd-gate.com/03/sinsei.cgi")
 
-    # regist parameter
-    agent.page.form.field_with(name: "raihoubi").value = recept_date
-    agent.page.form.field_with(name: "raihoujikan").value = recept_time
-    agent.page.form.checkbox_with(name: "seminarcheck").check # 団体受付可能フラグ
-    # slack id を会社名につけて登録結果メールに情報を引回す
-    # メール受信 webhook を受け取って、登録者にメンションするため
-    # 半角記号は使えない仕様なので削除する
-    agent.page.form.field_with(name: "kaisha[]").value = "【#{slack_id}】" + recept_company_name.gsub(/[[:punct:]]/, "")
-    agent.page.form.field_with(name: "mei[]").value = recept_visitor_name.gsub(/[[:punct:]]/, "")
-    agent.page.form.field_with(name: "kana[]").value = "カナ"
-    agent.page.form.field_with(name: "mail[]").value = ENV.fetch("MAIL_ADDRESS_WEBHOOK")
-    agent.page.form.field_with(name: "sinseiemail").value = ENV.fetch("MAIL_ADDRESS_HOST")
-    agent.page.form.field_with(name: "sinseitel").value = ENV.fetch("COMPANY_TEL")
+    # # regist parameter
+    # agent.page.form.field_with(name: "raihoubi").value = recept_date
+    # agent.page.form.field_with(name: "raihoujikan").value = recept_time
+    # agent.page.form.checkbox_with(name: "seminarcheck").check # 団体受付可能フラグ
+    # # slack id を会社名につけて登録結果メールに情報を引回す
+    # # メール受信 webhook を受け取って、登録者にメンションするため
+    # # 半角記号は使えない仕様なので削除する
+    # agent.page.form.field_with(name: "kaisha[]").value = "【#{slack_id}】" + recept_company_name.gsub(/[[:punct:]]/, "")
+    # agent.page.form.field_with(name: "mei[]").value = recept_visitor_name.gsub(/[[:punct:]]/, "")
+    # agent.page.form.field_with(name: "kana[]").value = "カナ"
+    # agent.page.form.field_with(name: "mail[]").value = ENV.fetch("MAIL_ADDRESS_WEBHOOK")
+    # agent.page.form.field_with(name: "sinseiemail").value = ENV.fetch("MAIL_ADDRESS_HOST")
+    # agent.page.form.field_with(name: "sinseitel").value = ENV.fetch("COMPANY_TEL")
 
-    # regist
-    agent.page.form.submit
+    # # regist
+    # agent.page.form.submit
 
     client = Slack::Web::Client.new(
       token: ENV.fetch("SLACK_TOKEN")
@@ -62,12 +64,12 @@ class ReceptionWorker
           fields: [
             {
               title: messages["intarctive"]["recept_name"],
-              value: "#{recept_company_name} #{recept_visitor_name} 様",
+              value: "#{dialog_result.company_name} #{dialog_result.visitor_name} 様",
               short: true
             },
             {
               title: messages["intarctive"]["recept_datetime"],
-              value: "#{recept_date} #{recept_time}",
+              value: "#{dialog_result.recept_date} #{dialog_result.recept_time}",
               short: true
             }
           ]
