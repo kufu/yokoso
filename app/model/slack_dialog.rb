5958bcdb544e17637e6c25cdba7a95aab3222556
yamashush <38120991+yamashush@users.noreply.github.com>
2022-12-31 14:11:25 +0900

    :recycle: Refactor time select options

--- app/model/slack_dialog.rb
+++ app/model/slack_dialog.rb
@@ -40,17 +40,12 @@ class SlackDialog
     end
   end
 
+  # @return [Array]
   def time_select_options
-    # Array: hh:mm
-    # 08:00, 08:30, ... , 21:00, 21:30
-    times = []
-    (8..21).each do |i|
-      hour = format("%.2d", i)
-      time_on = { label: "#{hour}:00", value: "#{hour}:00" }
-      time_half = { label: "#{hour}:30", value: "#{hour}:30" }
-      times.push(time_on)
-      times.push(time_half)
-    end
-    times
+    zero_padded_hours = (8..21).to_a.map { |n| format("%.2d", n) }
+    zero_padded_hours.map do |hour|
+      [{ label: "#{hour}:00", value: "#{hour}:00" },
+       { label: "#{hour}:30", value: "#{hour}:30" }]
+    end.flatten
   end
 end
