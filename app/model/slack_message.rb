3e50bf28b2ad35448f83666882aa965ed7761f94
yamashush <38120991+yamashush@users.noreply.github.com>
2022-12-31 19:22:46 +0900

    :seedling: Add attachement method

--- app/model/slack_message.rb
+++ app/model/slack_message.rb
@@ -2,6 +2,11 @@
 
 # @see 
 class SlackMessage
+  def attachments(color:, fields:)
+    { color: color,
+      fields: fields }
+  end
+
   def attachment_field(title:, value:, short: true)
     { title: title,
       value: value,
