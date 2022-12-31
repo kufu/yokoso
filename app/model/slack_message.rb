ed3bb96863cd3b3aa42f1bc733badee85826bc26
yamashush <38120991+yamashush@users.noreply.github.com>
2022-12-31 15:59:54 +0900

    :seedling: Add Slack message class

---
+++ app/model/slack_message.rb
@@ -0,0 +1,6 @@
+# frozen_string_literal: true
+
+# @see 
+class SlackMessage
+
+end
---
+++ spec/app/model/slack_message_spec.rb
@@ -0,0 +1,6 @@
+# frozen_string_literal: true
+
+require "spec_helper"
+require_relative "../../../app/model/slack_message"
+describe SlackMessage do
+end
