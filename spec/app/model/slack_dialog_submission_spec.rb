c37c46c8676b6370cfe77018b7bfa5ec934eafe5
yamashush <38120991+yamashush@users.noreply.github.com>
2022-12-31 16:25:30 +0900

    :white_check_mark: Add tests

--- spec/app/model/slack_dialog_submission_spec.rb
+++ spec/app/model/slack_dialog_submission_spec.rb
@@ -12,4 +12,22 @@ describe SlackDialogSubmittion do
       it { expect(instance.recept_date).to eq "2023/01/01" }
     end
   end
+  describe "#recept_time" do
+    context "ok" do
+      let(:post_body) { { submission: { time: "08:00" } } }
+      it { expect(instance.recept_time).to eq "08:00" }
+    end
+  end
+  describe "#company_name" do
+    context "ok" do
+      let(:post_body) { { submission: { company_name: "SmartHR" } } }
+      it { expect(instance.recept_time).to eq "SmartHR" }
+    end
+  end
+  describe "#visitor_name" do
+    context "ok" do
+      let(:post_body) { { submission: { name: "須磨 太郎" } } }
+      it { expect(instance.recept_time).to eq "須磨 太郎" }
+    end
+  end
 end
