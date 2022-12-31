637819af0655d3c15cf0eaac5e7a23393cb44d8e
yamashush <38120991+yamashush@users.noreply.github.com>
2022-12-31 16:30:37 +0900

    :seedling: Add initiailze

--- app/model/slack_dialog_submission.rb
+++ app/model/slack_dialog_submission.rb
@@ -2,5 +2,12 @@
 
 # @see https://api.slack.com/dialogs
 class SlackDialogSubmittion
+  def initialize(post_body)
+    @post_body = post_body
+  end
 
+  # @return [String]
+  def slack_user_id
+    @post_body.dig(:user, :id)
+  end
 end
