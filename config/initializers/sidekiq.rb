9161064efac307639c8a782809f4de9e2a3fe1f3
yamashush <38120991+yamashush@users.noreply.github.com>
2023-01-16 16:29:28 +0900

    :wrench: Update Redis setting for SSL

--- config/initializers/sidekiq.rb
+++ config/initializers/sidekiq.rb
@@ -1,9 +1,11 @@
 # frozen_string_literal: true
 
 Sidekiq.configure_server do |config|
-  config.redis = { url: ENV.fetch("REDIS_URL") }
+  config.redis = { url: ENV.fetch("REDIS_URL", nil),
+                   ssl_params: { verify_mode: OpenSSL::SSL::VERIFY_NONE } }
 end
 
 Sidekiq.configure_client do |config|
-  config.redis = { url: ENV.fetch("REDIS_URL") }
+  config.redis = { url: ENV.fetch("REDIS_URL", nil),
+                   ssl_params: { verify_mode: OpenSSL::SSL::VERIFY_NONE } }
 end
