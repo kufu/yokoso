acb3105277e3858f5c945a07dfac768874b3c218
yamashush <38120991+yamashush@users.noreply.github.com>
2023-04-12 06:14:27 +0900

    :bug: Fix arg

--- app/models/chat_message_sender.rb
+++ app/models/chat_message_sender.rb
@@ -10,7 +10,7 @@ class ChatMessageSender
   # @param post_body [Hash]
   # @see https://api.slack.com/methods/chat.postMessage
   # @see https://github.com/slack-ruby/slack-ruby-client/blob/master/lib/slack/web/api/endpoints/chat.rb
-  def post_public_message
+  def post_public_message(post_body)
     @slack_api_client.chat_postMessage(post_body)
   end
 
